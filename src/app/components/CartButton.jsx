8c602e25296f119d6f2ca8bd26dd80ba30244ba4
gautampar <grajput081@gmail.com>
2025-10-06 19:43:24 +0530

    project deployed

---
+++ src/app/components/CartButton.jsx
@@ -0,0 +1,35 @@
+
+import { addToCart } from "@/api/auth";
+import Image from "next/image";
+import { toast } from "react-toastify";
+
+
+const CartButton = ({ productId, quantity = 1 }) => {
+  const handleAddToCart = async () => {
+    try {
+      const result = await addToCart(productId, "", quantity);
+      if (result?.data?.products) {
+        localStorage.setItem("userCart", JSON.stringify(result.data.products));
+      }
+      // console.log("Cart Response:", result);
+      toast.success("Cart fetched successfully!");
+    } catch (err) {
+      // console.error("Cart Error:", err);
+      toast.error(err?.error?.message);
+    }
+  };
+
+  return (
+    <button onClick={handleAddToCart}>
+      <Image
+        src="/images/home/cart.svg"
+        alt="cart"
+        height={40}
+        width={40}
+        className="object-cover md:h-[52px] md:w-[52px]"
+      />
+    </button>
+  );
+};
+
+export default CartButton;
